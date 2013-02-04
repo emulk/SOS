d2fe900b8e9f42a49f9175f234e010740f9e3059
emulk <orgest.shehaj@gmail.com>
2013-02-04 15:43:48 +0100

    other updates

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -27,6 +27,36 @@ HIDDEN pcb_t pcb_table[MAXPROC];
  * E la testa della lista pcbFree */ 
 HIDDEN struct list_head pcbfree_h;
 
+HIDDEN void initPcb_t (pcb_t *p) {
+	/*process queue fields */
+ 	p->p_next.next = NULL ;
+ 	p->p_next.prev = NULL ;
+		/*process tree fields */
+	p->p_parent = NULL;
+	INIT_LIST_HEAD (&(p->p_child));
+	p->p_sib.next = NULL;
+	p->p_sib.prev = NULL;
+
+	/* processor state, etc 
+   * la struttura dati state_t e' una struttura contenente dati di tipo U32 che sono un unsigned int della macchina su cui si trova
+   */
+	p->p_s.entry_hi = 0;
+	p->p_s.cause = 0;
+	p->p_s.status = 0;
+	p->p_s.pc_epc = 0;  /* pc in the new area, epc in the old area */
+	p->p_s.gpr[29] = 0;
+	p->p_s.hi = 0;
+	p->p_s.lo = 0;				
+
+	/* process priority */
+	p->priority = 0;
+	
+	/* key of the semaphore on which the process is eventually blocked */
+	p->p_semkey = 0;
+
+}
+ 	
+
 /**
  * TODO
  * Inizializza la lista dei processi liberi in modo da contenere
@@ -46,15 +76,22 @@ void initPcbs(void)
 	}
 }
 
-/* TODO */
-void freePcb(struct pcb_t *p) { }
+/*[2]
+ * Descrizione: Inserisce il PCB puntato da p 
+ * nella lista dei pcb liberi (pcbFree)
+ * @return: l'elemento inserito
+ * list_add: si trova in listx.h */
+void freePcb(struct pcb_t *p) { 
+	return list_add(&(p->p_next), &pcbFree_h);
+}
 
 /**
- * TODO
- * Restituisce NULL se la pcbFree e' vuota. Altrimenti rimuove un elemento
+ * [3]
+ * Descrizione: Restituisce NULL se la pcbFree e' vuota. Altrimenti rimuove un elemento
  * dalla pcbFree, inizializza tutti i campi (NULL/0) e restituisce l'elemento
  * rimosso.
  * @return: NULL || l'elemento rimosso
+ * list_empty:si trova in listx.h
  **/
 struct pcb_t *allocPcb(void)
 {
@@ -65,7 +102,10 @@ struct pcb_t *allocPcb(void)
 	if(list_empty(&pcbfree_h)){
 		return NULL;
 	} else {
-		;
+		temp = container_of (pcbFree_h.next, pcb_t, p_next); /* estrae il primo elemento della lista e lo salva in p */
+		list_del (pcbFree_h.next); /* elimina l'elemento estratto dalla lista'*/
+		initPcb_t(temp); /* inizializza il pcb */
+		return temp; /* ritorna il pcb rimosso */
 	}
 }
 
