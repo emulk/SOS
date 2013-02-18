cb6b85d0545552af5e1200949e54f682345e041f
fmp196 <fmp196@gmail.com>
2013-02-18 13:13:09 +0100

    allocPcb() OK

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -23,9 +23,45 @@
 // funzione di stampa per il debug
 extern void addokbuf(char *str);
 
-static pcb_t pcb_table[MAXPROC];
-static struct pcb_t **pcbfree_h;
- 	
+static pcb_t pcb_table[MAXPROC+1];
+static struct pcb_t *pcbfree_h;
+
+static int initialized = FALSE;
+ 
+void initPcb(struct pcb_t *p)
+{
+	int count = 0;
+
+ 	p->p_next = NULL ;
+	p->p_first_child = NULL;
+	p->p_parent = NULL;
+	p->p_sib = NULL;
+	
+	// inizializzazione campi processo
+	p->p_next = NULL;
+	p->p_parent = NULL;
+	p->p_first_child = NULL;
+	p->p_sib = NULL;
+		
+	// inizializzazione dei campi del processo
+	p->p_s.entry_hi = 0;
+	p->p_s.cause    = 0;
+	p->p_s.status   = 0;
+	p->p_s.pc_epc   = 0;
+	p->p_s.hi       = 0;
+	p->p_s.lo       = 0;
+
+	// inizializzazione registri ad uso generale
+	for (count=0; count<29; count++)
+		p->p_s.gpr[count] = 0;
+
+	// inizializzazione priorita' processo
+	p->priority = 0;
+
+	// inizializzazione chiave del semaforo 
+	// new->p_semkey = NULL;
+} 
+
 
 /**
  * Inizializzazione lista processi liberi e della memoria contenente
@@ -35,10 +71,21 @@ void initPcbs(void)
 {
 	static int cont = 0;
 	
-	struct pcb_t *new = &(pcb_table[cont++]);
-	freePcb(new);
+	if (initialized == FALSE)
+	{
+		pcbfree_h = &(pcb_table[MAXPROC]);
+		pcbfree_h->p_next = pcbfree_h;
+		pcbfree_h->p_parent = NULL;
+		pcbfree_h->p_sib = NULL;
+		pcbfree_h->p_first_child = NULL;
+		
+		initialized = TRUE;
+	}
 	
-	if (cont == MAXPROC)
+	struct pcb_t *new = &(pcb_table[cont]);
+	freePcb(new);
+
+	if (++cont == MAXPROC)
 		return;
 	else
 		initPcbs();
@@ -51,8 +98,8 @@ void initPcbs(void)
  */
 void freePcb(struct pcb_t *p)
 {
-	p->p_next = (*pcbfree_h)->p_next;
-	(*pcbfree_h)->p_next = p;
+	p->p_next = pcbfree_h->p_next;
+	pcbfree_h->p_next = p;
 }
 
 /**
@@ -64,43 +111,18 @@ void freePcb(struct pcb_t *p)
  **/
 struct pcb_t *allocPcb(void)
 {
-	int count = 0;
 	struct pcb_t *new = NULL;
-	
-	if ((*pcbfree_h)->p_next != (*pcbfree_h))
+
+	if ((pcbfree_h)->p_next != (pcbfree_h))
 	{
 		// punto al primo processo in testa alla lista
-		new = &(*pcbfree_h)->p_next;
+		new = (pcbfree_h)->p_next;
 		
 		// rimozione del processo dalla lista
-		(*pcbfree_h)->p_next = &new->p_next;
-		
-		// inizializzazione campi processo
-		new->p_next = NULL;
-		new->p_parent = NULL;
-		new->p_first_child = NULL;
-		new->p_sib = NULL;
-		
-		// inizializzazione dei campi del processo
-		new->p_s.entry_hi = 0;
-		new->p_s.cause    = 0;
-		new->p_s.status   = 0;
-		new->p_s.pc_epc   = 0;
-		new->p_s.hi       = 0;
-		new->p_s.lo       = 0;
-
-		// inizializzazione registri ad uso generale
-		for (count=0; count<29; count++)
-			new->p_s.gpr[count] = 0;
-
-		// inizializzazione priorita' processo
-		 new->priority = 0;
-
-		/* inizializzazione chiave del semaforo in cui il
-		 * processo e' bloccato */
-		// new->p_semkey = NULL;
+		(pcbfree_h)->p_next = new->p_next;
+		initPcb(new);
 	}
-	
+
 	return new;
 }
 
@@ -113,24 +135,39 @@ struct pcb_t *allocPcb(void)
  */
 void insertProcQ(struct pcb_t **head, struct pcb_t *p)
 {
+	if ((*head) != NULL)
+	{
+		if ((*head)->priority > p->priority)	
+			insertProcQ(&(*head)->p_next, p);
+		else
+		{
+    		p->p_next = (*head)->p_next;
+    		(*head)->p_next = p;
+		}
+	}
 }
 
-/*[5]
- * Descrizione: Restituisce l'elemento di testa della coda dei processi puntata da head,
+/**
+ * Restituisce l'elemento di testa della coda dei processi puntata da head,
  * senza rimuoverlo, ritorna null se la coda e vuota.
- * return: null || l'elemento di testa*/
+ * @return: NULL se la lista e' vuota, oppure l'elemento estratto.
+ */
 struct pcb_t* headProcQ(struct pcb_t* head)
 {
-	return NULL;
-}
+	struct pcb_t *tmp = NULL;
+	if (head != NULL)
+		tmp = head->p_next;
 	
+	return tmp;
+}
+
 /*[6]
  * Descrizione: rimuove il primo elemento dalla coda dei processi puntata da head.
  * Ritorna null se la coda e vuota, altrimenti ritorna il puntatore all'elemento
  * rimosso dalla lista
  * return: NULL || puntatore all'elemento rimosso*/
 struct pcb_t* removeProcQ(struct pcb_t** head)
-{ 
+{
 	return NULL;
 }
 
@@ -144,14 +181,14 @@ struct pcb_t* outProcQ(struct pcb_t** head, struct pcb_t *p)
 	return NULL;
 }
 
-/*[8]
+/* [8]
  * Descrizione: Inserisce il pcb puntato da p come figlio del pcb puntato da parent*/
 void insertChild(struct pcb_t *parent, struct pcb_t *p)
 {
 	return NULL;
 }
 
-/*[9]
+/* [9]
  * Descrizione: Rimuove il primo figlio del pcb puntato da p, se p non ha figli restituisce nulll
  * return: NULL || primo figlio*/
 struct pcb_t* removeChild(struct pcb_t *p)
@@ -159,12 +196,12 @@ struct pcb_t* removeChild(struct pcb_t *p)
 	return NULL;
 }
 
-/*[10]
+/* [10]
  * Descrizione:
  * Rimuove il PCB puntato da p dalla lista dei figli del padre. Se il PCB puntato da p non ha un padre,
-*  restituisce NULL. Altrimenti restituisce l'elemento rimosso (cioe' p). A differenza della removeChild, 
-* p puo' trovarsi in una posizione arbitraria (ossia non e' necessariamente il primo figlio del padre)
-* return: NULL || figlio*/
+ * restituisce NULL. Altrimenti restituisce l'elemento rimosso (cioe' p). A differenza della removeChild, 
+ * p puo' trovarsi in una posizione arbitraria (ossia non e' necessariamente il primo figlio del padre)
+ * return: NULL || figlio*/
 struct pcb_t* outChild(struct pcb_t* p)
 {
 	return NULL; 
